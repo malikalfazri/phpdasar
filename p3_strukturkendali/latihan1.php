efc5855ba423c3da9219d0d9077e6326b76fdea1
ikhsanrizal <isanrizal61@gmail.com>
2020-06-04 20:30:59 +0700

    syntaks dasar & struktur kendali

---
+++ p3_strukturkendali/latihan1.php
@@ -0,0 +1,21 @@
+<?php
+// pengulangan
+// for 
+// while 
+// do..while
+
+// for ($i = 0; $i < 100; $i++) {
+//   echo "hello world";
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//   echo "hellor world <br>";
+//   $i++;
+// }
+
+$i = 0;
+do {
+  echo "Hello World <br>";
+  $i++;
+} while ($i < 5);
